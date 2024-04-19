0d85f6639ab5f6fc8c871b2be9ec9f60ee76fa7b
mulyadirizki <mulyadirizkiputra10@gmail.com>
2024-04-19 10:05:12 +0700

    update

--- resources/views/koffe/frontend/default.blade.php
+++ resources/views/koffe/frontend/default.blade.php
@@ -3,7 +3,7 @@
 <html lang="en">
   <!-- [Head] start -->
   <head>
-    <title>Moel</title>
+    <title>MA-Pos</title>
     <!-- [Meta] -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">

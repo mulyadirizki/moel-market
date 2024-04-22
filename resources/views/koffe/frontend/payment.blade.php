65f9f84f31ecdad64b91a1fbb9640fd4ad757cd6
mulyadirizki <mulyadirizkiputra10@gmail.com>
2024-04-22 12:12:16 +0700

    update

--- resources/views/koffe/frontend/payment.blade.php
+++ resources/views/koffe/frontend/payment.blade.php
@@ -134,12 +134,14 @@
 
     function changePembayaran() {
       let statusbayar = $('#statusbayar').val();
-      if (statusbayar == 2) {
+      if (statusbayar == 2 || statusbayar == 3) {
           $('#nmpiutang')[0].removeAttribute("hidden")
           $('#priceitem')[0].setAttribute("hidden", true)
+          $('.btn-bayar').removeAttr('disabled');
       } else {
           $('#nmpiutang')[0].setAttribute("hidden", true)
           $('#priceitem')[0].removeAttribute("hidden")
+          $('.btn-bayar').attr('disabled', 'disabled');
       }
     }
 
